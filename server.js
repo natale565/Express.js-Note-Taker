b43aaca448acd87f11441b545f820c0849a4bb2f
Chris Natale <natale565@icloud.com>
2024-08-15 11:15:19 -0400

    updated server JS

--- server.js
+++ server.js
@@ -8,29 +8,29 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static('Develop/public'));
+app.use(express.static('public'));
 
 // GET route for HTML
-app.get('/'), (req, res =>
-    res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // GET route for notes.HTML
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
-app.get("/api/notes", function (req, res) {
-    fs.readFile("Develop/db/db.json", "utf8", (err, data) => {
+app.get('/api/notes', function (req, res) {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
       var jsonData = JSON.parse(data);
       console.log(jsonData);
       res.json(jsonData);
     });
   });
 
-  app.post("/api/notes", function (req, res) {
-    fs.readFile("Develop/db/db.json", "utf8", (err, data) => {
+  app.post('/api/notes', function (req, res) {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
         if (err) {
             console.error(err);
             return res.status(500).send("Error reading the database.");
@@ -42,7 +42,7 @@ app.get("/api/notes", function (req, res) {
 
         jsonData.push(newNote);
 
-        fs.writeFile("Develop/db/db.json", JSON.stringify(jsonData, null, 2), (err) => {
+        fs.writeFile('./db/db.json', JSON.stringify(jsonData, null, 2), (err) => {
             if (err) {
                 console.error(err);
                 return res.status(500).send("Error writing to the database.");
@@ -52,6 +52,30 @@ app.get("/api/notes", function (req, res) {
         });
     });
 });
+
+
+// DELETE route for notes
+app.delete("/api/notes/:id", (req, res) => {
+    const id = req.params.id;
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Error reading the database.");
+      }
+  
+      let parsedData = JSON.parse(data);
+      const filterData = parsedData.filter(note => note.id !== id);
+  
+      fs.writeFile('./db/db.json', JSON.stringify(filterData, null, 2), (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send("Error writing to the database.");
+        }
+  
+        res.send(`Deleted note with id ${id}`);
+      });
+    });
+  });
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
